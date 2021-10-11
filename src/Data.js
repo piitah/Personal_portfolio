6df2c42187fa0b776f24161f25ee35c2d28800fa
peter <peterakintayo838@yahoo.com>
2021-10-11 19:49:46 +0100

    changes

--- src/Data.js
+++ src/Data.js
@@ -3,28 +3,28 @@ export const datas = [
     {
         title : "E-Commerce",
         desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nesciunt eaque aliquid doloremque id consequatur ratione. Modi, odio possimus",
-        img: "img1",
+        img: require("./assets/bg8.jpg"),
         stacks: ["Vue", "Vuex", "Node", "MongoBb"],
         link: "sssss"
     },
     {
         title : "E-Commerce",
         desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nesciunt eaque aliquid doloremque id consequatur ratione. Modi, odio possimus",
-        img: "img1",
+        img: require("./assets/bg1.jpg"),
         stacks: ["Vue", "Vuex", "Node", "MongoBb"],
         link: "sssss"
     },
     {
         title : "E-Commerce",
         desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nesciunt eaque aliquid doloremque id consequatur ratione. Modi, odio possimus",
-        img: "img1",
+        img: require("./assets/bg6.jpg"),
         stacks: ["Vue", "Vuex", "Node", "MongoBb"],
         link: "sssss"
     },
     {
         title : "E-Commerce",
         desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nesciunt eaque aliquid doloremque id consequatur ratione. Modi, odio possimus",
-        img: "img1",
+        img: require("./assets/bg4.jpg"),
         stacks: ["Vue", "Vuex", "Node", "MongoBb"],
         link: "sssss"
     }
