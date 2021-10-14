4f333db6270472ad9779ba3b72b3a15180d2a7e6
peter <peterakintayo838@yahoo.com>
2021-10-14 16:32:48 +0100

    changes

---
+++ src/components/skills.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Skills = (props) => {
+    return (
+        <React.Fragment>
+            <div style={{backgroundColor:"red", width: "100%", height:"80px"}}>
+
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Skills
